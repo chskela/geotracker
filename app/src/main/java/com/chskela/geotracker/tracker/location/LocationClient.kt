7b4d03d587412e444548e719c09b51baf74c481c
Chistyakov Alexey <chskela@gmail.com>
2022-10-18 23:29:56 +0300

    Create LocationClient.kt

---
+++ app/src/main/java/com/chskela/geotracker/tracker/location/LocationClient.kt
@@ -0,0 +1,10 @@
+package com.chskela.geotracker.tracker.location
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdate(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+}
