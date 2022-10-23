885a4676d57720f7245d19e71f33c0cc09f84a24
Chistyakov Alexey <chskela@gmail.com>
2022-10-23 23:31:55 +0300

    Updated MainActivity.kt

--- app/src/main/java/com/chskela/geotracker/MainActivity.kt
+++ app/src/main/java/com/chskela/geotracker/MainActivity.kt
@@ -1,43 +1,41 @@
 package com.chskela.geotracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.chskela.geotracker.tracker.presentation.MainScreen
+import com.chskela.geotracker.tracker.presentation.MainScreenViewModel
+import com.chskela.geotracker.tracker.service.LocationService
 import com.chskela.geotracker.ui.theme.GeoTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                LocationService.CHANNEL_ID, "location", NotificationManager.IMPORTANCE_LOW
+            )
+        )
+//        onRequestPermissionsResult()
+        val mainScreenViewModel: MainScreenViewModel by viewModels()
         setContent {
             GeoTrackerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(mainScreenViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GeoTrackerTheme {
-        Greeting("Android")
     }
 }
