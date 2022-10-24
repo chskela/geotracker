debe9ea42aa79fa1737f3e56a8fc695ced4cf553
Chistyakov Alexey <chskela@gmail.com>
2022-10-24 19:55:05 +0300

    Created LocationDao

---
+++ app/src/main/java/com/chskela/geotracker/tracker/data/LocationDao.kt
@@ -0,0 +1,15 @@
+package com.chskela.geotracker.tracker.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface LocationDao {
+
+    @Insert
+    fun save(location: Location)
+
+    @Query("SELECT * FROM location WHERE is_send = 0")
+    fun getListLocationNotSend()
+}
