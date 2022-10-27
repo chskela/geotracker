10a55eb303f20fea728f09a2c17a60c2156bfaaf
Chistyakov Alexey <chskela@gmail.com>
2022-10-27 15:17:33 +0300

    Updated AppDatabase  - add .fallbackToDestructiveMigration()

--- app/src/main/java/com/chskela/geotracker/tracker/data/AppDatabase.kt
+++ app/src/main/java/com/chskela/geotracker/tracker/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Location::class], version = 1)
+@Database(entities = [Location::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun locationDao(): LocationDao
 
@@ -19,7 +19,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "location_db"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
