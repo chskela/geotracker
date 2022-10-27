5f65e589766b78b9fa8339693730c0ed9cc57dd9
Chistyakov Alexey <chskela@gmail.com>
2022-10-27 19:14:53 +0300

    Updated LocationDao  - add fun update

--- app/src/main/java/com/chskela/geotracker/tracker/data/LocationDao.kt
+++ app/src/main/java/com/chskela/geotracker/tracker/data/LocationDao.kt
@@ -3,12 +3,16 @@ package com.chskela.geotracker.tracker.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface LocationDao {
 
     @Insert
     fun save(location: Location)
+
+    @Update
+    fun update(vararg locations: Location)
 
     @Query("SELECT * FROM location WHERE is_send = 0")
     fun getListLocationNotSend() : List<Location>
