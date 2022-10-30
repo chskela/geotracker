df5752380fa9911ba680169828510831e8e82be8
Chistyakov Alexey <chskela@gmail.com>
2022-10-30 17:57:00 +0300

    Updated LocationService - add restApiService, create fun sendLocationData

--- app/src/main/java/com/chskela/geotracker/tracker/service/LocationService.kt
+++ app/src/main/java/com/chskela/geotracker/tracker/service/LocationService.kt
@@ -1,22 +1,25 @@
 package com.chskela.geotracker.tracker.service
 
+import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.icu.util.Calendar
 import android.os.IBinder
+import android.provider.Settings.Secure
 import androidx.core.app.NotificationCompat
 import com.chskela.geotracker.GeoTrackerApplication
 import com.chskela.geotracker.R
 import com.chskela.geotracker.tracker.data.Location
 import com.chskela.geotracker.tracker.data.LocationRepository
+import com.chskela.geotracker.tracker.data.mapper.toLocationDto
 import com.chskela.geotracker.tracker.location.DefaultLocationClient
 import com.chskela.geotracker.tracker.location.LocationClient
+import com.chskela.geotracker.tracker.network.RestApiService
+import com.chskela.geotracker.tracker.utils.Settings.LOCATION_SEND
+import com.chskela.geotracker.tracker.utils.Settings.LOCATION_UPDATE
 import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -25,7 +28,8 @@ import java.util.*
 
 class LocationService : Service() {
 
-    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private val serviceScope by lazy { CoroutineScope(SupervisorJob() + Dispatchers.IO) }
+    private val restApiService by lazy { RestApiService() }
     private lateinit var locationClient: LocationClient
     private lateinit var repository: LocationRepository
 
@@ -55,6 +59,7 @@ class LocationService : Service() {
         stopSelf()
     }
 
+    @SuppressLint("HardwareIds")
     private fun start() {
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Tracking location")
@@ -64,14 +69,14 @@ class LocationService : Service() {
 
         val notificationManager = getSystemService(NotificationManager::class.java)
 
-        locationClient.getLocationUpdate(5000L)
+        locationClient.getLocationUpdate(LOCATION_UPDATE)
             .catch { e -> e.printStackTrace() }
             .onEach { location ->
                 val dateTime = formatDate(Calendar.getInstance().time)
                 val latitude = location.latitude
                 val longitude = location.longitude
                 val candidate = Location(
-                    uidPhone = "test",
+                    uidPhone = Secure.getString(contentResolver, Secure.ANDROID_ID),
                     date = dateTime,
                     latitude = latitude,
                     longitude = longitude
@@ -85,12 +90,35 @@ class LocationService : Service() {
             }
             .launchIn(serviceScope)
 
+        sendLocationData()
+
         startForeground(NOTIFICATION_ID, notification.build())
     }
 
     override fun onDestroy() {
         super.onDestroy()
         serviceScope.cancel()
+    }
+
+    private fun sendLocationData() = serviceScope.launch {
+        while (true) {
+            val listLocation = repository.getListLocationNotSend()
+            var isSend = false
+            restApiService.sendLocation(listLocation.map { it.toLocationDto() }){
+                isSend = it != null
+            }
+
+
+            if (listLocation.isNotEmpty() && isSend) {
+                markDataAsSubmitted(listLocation)
+            }
+            delay(LOCATION_SEND)
+        }
+    }
+
+    private fun markDataAsSubmitted(listLocation: List<Location>) {
+        repository.update(*listLocation.map { location -> location.copy(isSend = true) }
+            .toTypedArray())
     }
 
     private fun formatDate(date: Date) =
--- app/src/main/java/com/chskela/geotracker/tracker/utils/Settings.kt
+++ app/src/main/java/com/chskela/geotracker/tracker/utils/Settings.kt
@@ -3,5 +3,5 @@ package com.chskela.geotracker.tracker.utils
 object Settings {
     const val LOCATION_UPDATE = 1000L * 5
     const val LOCATION_SEND = 1000L * 60 * 5
-    const val URI = "test"
+    const val BASE_URL = "test"
 }
